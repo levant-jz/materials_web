ee42e7a37d0b348cc7dae2fed5acb9455e40c1cd
Jiahao Zhang <jiahaozhang@Jiahaos-MacBook-Pro.local>
2021-05-02 20:27:19 +0800

    add karel reader

---
+++ karel-reader/docs/karel/js/ide/action.js
@@ -0,0 +1,12 @@
+// Action class	
+function Action(id, parameter) {
+
+    this.getId = function() {
+        return id;
+    }
+
+    this.getParam = function() {
+
+        return parameter;
+    }
+}
---
+++ karel-reader/docs/karel/lib/browser_detect.js
@@ -0,0 +1,6 @@
+(function() {
+    var ua = $.browser;
+    if (ua.msie && parseInt($.browser.version, 10) < 9) {
+        window.location.href = "./error/unsupported_browser.html";
+    }
+})();
